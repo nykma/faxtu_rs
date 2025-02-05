f364fdbd198b23e7ad554da532e15275d13a517e
Nyk Ma <nykma@mask.io>
2025-02-06 00:48:47 +0800

    [+] Logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use actix_web::{App, HttpServer};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::EnvFilter;
 
 mod api;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server at 127.0.0.1:8100");
-    // Print current process's PID
-    println!("PID: {}", std::process::id());
+    // Log system
+    let log_subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy()
+                .add_directive("actix=debug".parse().unwrap())
+                .add_directive("tokio=info".parse().unwrap()),
+        )
+        .finish();
+
+    tracing::subscriber::set_global_default(log_subscriber)
+        .expect("Setting default subscriber failed");
 
     HttpServer::new(|| {
         App::new()
